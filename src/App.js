527395e6c7b7eba49da04e5063979baace4cb3b0
fdavison1 <fred.noreply@gmail.com>
2019-11-27 14:13:55 -0700

    initial set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components'
 import './App.css';
+
+const Header = styled.div`
+display: flex
+align-items: center
+justify-content: center
+height: 100vh`
+
+const Title = styled.h1`
+font-family: sans-serif
+font-weight: 200`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header>
+       
+
+      <Title>Fred Davison</Title>
+
+
+      </Header>
     </div>
   );
 }
