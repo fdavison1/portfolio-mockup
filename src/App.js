3d749cff3da3f8b75e5ee4a2a99eb2ab9e904089
fdavison1 <fred.noreply@gmail.com>
2019-11-27 15:25:59 -0700

    layout 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import styled from 'styled-components'
-import './App.css';
+import './App.css'
 
 const Header = styled.div`
 display: flex
+flex-direction: column
 align-items: center
 justify-content: center
-height: 100vh`
+height: 100vh
+margin: 0`
 
 const Title = styled.h1`
-font-family: sans-serif
-font-weight: 200`
+font-family: 'Ubuntu', sans-serif;
+font-weight: 400
+font-size: 5.5rem
+background-color: pink
+border: solid 2px pink
+padding: 5px
+color: #262626
+opacity: 1
+z-index: 10
+width: 100vw`
 
 function App() {
   return (
@@ -19,6 +29,7 @@ function App() {
        
 
       <Title>Fred Davison</Title>
+      <p>Full-stack web developer</p>
 
 
       </Header>
