88937111d378763fe8249cb1c978fef742b8de23
fdavison1 <fred.noreply@gmail.com>
2019-11-27 18:01:44 -0700

    sections

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import styled from 'styled-components'
 import './App.css'
 
 const Title = styled.h1`
-font-family: 'Ubuntu', sans-serif;
+font-family: 'Poppins', sans-serif;
 font-weight: 700
 padding: 0
 margin: 0
-font-size: 8.5rem
+font-size: 8rem
+opacity: 1`
+
+const Title2 = styled.h1`
+font-family: 'Poppins', sans-serif;
+font-weight: 700
+padding: 0
+margin: 0
+font-size: 5rem
 opacity: 1`
 
 const SubTitle = styled.h3`
@@ -15,7 +23,6 @@ font-size: 2.5rem
 margin: 0
 font-family: sans-serif
 font-weight: 300
-
 `
 
 const TitleDiv = styled.div`
@@ -25,9 +32,15 @@ opacity: .9
 text-shadow: 1px 1px #262626
 `
 
-const Div = styled.div`
+const Div1 = styled.div`
 height: 100vh
+padding: 50px
 background: white`
+
+const Div2 = styled.div`
+height: 100vh
+padding: 50px
+background: rgb(161,161,161)`
 
 function App() {
   return (
@@ -45,7 +58,14 @@ function App() {
 
 
       </header>
-      <Div></Div>
+      <Div1>
+        <Title2>about</Title2>
+      </Div1>
+
+      <Div2>
+        <Title2>portfolio</Title2>
+      </Div2>
+
     </div>
   );
 }
