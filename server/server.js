99f23ed64ef254008dda07a9554c9b733dfaef3a
fdavison1 <fred.noreply@gmail.com>
2019-12-09 21:08:36 -0700

    build folder

--- server/server.js
+++ server/server.js
@@ -5,4 +5,8 @@ const { SERVER_PORT } = process.env
 const app = express()
 app.use(express.json())
 
+const path = require('path'); // Usually moved to the start of file
+
+app.use( express.static( `${__dirname}/../build` ) );
+
 app.listen(SERVER_PORT, () => console.log(`port ${SERVER_PORT} is on and ready to copy`))
