80a060c5c91d3e59e6911f02cc2091c627434d3b
fdavison1 <fred.noreply@gmail.com>
2019-12-09 21:05:17 -0700

    server

---
+++ server/server.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+const express = require('express')
+const { SERVER_PORT } = process.env
+
+const app = express()
+app.use(express.json())
+
+app.listen(SERVER_PORT, () => console.log(`port ${SERVER_PORT} is on and ready to copy`))
