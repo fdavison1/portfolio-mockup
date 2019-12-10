79c23ee884df960b134d7b2b11ec87bb967a4812
fdavison1 <fred.noreply@gmail.com>
2019-12-10 05:34:36 -0700

    noopener noreferrer

--- src/App.js
+++ src/App.js
@@ -235,11 +235,11 @@ function App() {
         <AboutDiv>
           <Banner2>
           <MobileDiv>
-          <a href='https://fredlist.app/' target='_blank'>
+          <a href='https://fredlist.app/' target='_blank' rel="noopener noreferrer">
           <Thumbnail src={Landing} />
           </a>
           <ListApp>
-          <A href='https://fredlist.app/' target='_blank'>
+          <A href='https://fredlist.app/' target='_blank' rel="noopener noreferrer">
           <SubTitle>List App</SubTitle>
           </A>
           <Paragraph>A simple list app designed to demonstrate skills and technologies learned while participating in an immersive 13-week web development program at DevMountain.</Paragraph>
