8f93d7169812d833151b066f810005c8fb85890a
fdavison1 <fred.noreply@gmail.com>
2019-12-16 19:42:56 -0700

    skills section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Landing from './assets/screenshots/landing.png'
 // import Dash from './assets/screenshots/dash.png'
 import Headshot from './assets/Fred.jpg'
 import Footer from './Footer'
+import html from './assets/HTML5_Badge.svg'
+import css from './assets/css3.png'
 
 const Title = styled.h1`
 color: #262626
@@ -178,7 +180,7 @@ background: #6e7889
     width: 400px
   }`
 
-  const Break =styled.br`
+const Break = styled.br`
   display: none
   @media (max-width: 500px){
     display: block
@@ -189,8 +191,6 @@ const ListApp = styled.div`
   text-align: center
 }`
 
-
-
 const MobileDiv = styled.div`
 display: flex
 align-items: center
@@ -198,17 +198,41 @@ align-items: center
   flex-direction: column
 }`
 
+const BoxContainer = styled.div`
+display: flex
+flex-wrap: wrap
+justify-content: center`
+
+const Box = styled.div`
+display: flex
+justify-content: center
+flex-direction: column
+align-items: center
+border: 1px solid #999999
+height: 250px
+width: 250px
+margin: 10px
+border-radius: 3px
+background: lightgray
+`
+
+const BoxTitle = styled.div`
+font-size: 2rem
+font-family: sans-serif
+font-weight: 200
+`
+
 function App() {
   return (
     <div className="App">
       <header>
         <nav>
-          {/* <A href='#about'>
+          <A href='#about'>
             <SubTitle>about</SubTitle>
           </A>
           <A href='#portfolio'>
             <SubTitle>portfolio</SubTitle>
-          </A> */}
+          </A>
 
         </nav>
         <Icons>
@@ -231,46 +255,94 @@ function App() {
       </header>
 
       <Div1>
+        <Title2>skills</Title2>
+        <BoxContainer>
+          <Box>
+            <BoxTitle>HTML</BoxTitle>
+            <img src={html} alt="HTML logo" />
+          </Box>
+          <Box>
+            <BoxTitle>CSS</BoxTitle>
+            <img src={css} alt="CSS logo" />
+          </Box>
+          <Box>
+            <BoxTitle>Javascript</BoxTitle>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png" alt="Javascript logo" />
+          </Box>
+          <Box>
+            <BoxTitle>React</BoxTitle>
+            <img src="https://cdn.worldvectorlogo.com/logos/react.svg" alt="react logo" />
+          </Box>
+          <Box>
+            <BoxTitle>Redux</BoxTitle>
+            <img src="https://raw.githubusercontent.com/reduxjs/redux/master/logo/logo.png" alt="Redux logo"/>
+
+          </Box>
+          <Box>
+            <BoxTitle>PostgreSQL</BoxTitle>
+            <img src="https://portworx.com/wp-content/uploads/2018/05/postgresql-logo.png" alt="PostgreSQL" />
+          </Box>
+          <Box>
+            <BoxTitle>MongoDB</BoxTitle>
+            <img src="https://miro.medium.com/max/552/1*NRCOyVBY6Jiqr4Q9A1zoaQ.png" alt="MongoDB logo" />
+          </Box>
+          <Box>
+            <BoxTitle>Jest / Enzyme</BoxTitle>
+            <img src="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/940/full/jestlogo.png" alt="Jest logo" />
+          </Box>
+        </BoxContainer>
+      </Div1>
+
+      <Div1>
         <A name='about'></A>
         <Title2>portfolio</Title2>
         <AboutDiv>
           <Banner2>
-          <MobileDiv>
-          <a href='https://fredlist.app/' target='_blank' rel="noopener noreferrer">
-          <Thumbnail src={Landing} />
-          </a>
-          <ListApp>
-          <A href='https://fredlist.app/' target='_blank' rel="noopener noreferrer">
-          <SubTitle id='fredlist'>fredlist.app</SubTitle>
-          </A>
-          <Paragraph>A simple list app designed to demonstrate skills and technologies learned while participating in an immersive 13-week web development program at DevMountain.</Paragraph>
-          </ListApp>
-          </MobileDiv>
-        </Banner2>
+            <MobileDiv>
+              <a href='https://fredlist.app/' target='_blank' rel="noopener noreferrer">
+                <Thumbnail src={Landing} />
+              </a>
+              <ListApp>
+                <A href='https://fredlist.app/' target='_blank' rel="noopener noreferrer">
+                  <SubTitle id='fredlist'>fredlist.app</SubTitle>
+                </A>
+                <Paragraph>A simple list app designed to demonstrate skills and technologies learned while participating in an immersive 13-week web development program at DevMountain.</Paragraph>
+              </ListApp>
+            </MobileDiv>
+          </Banner2>
         </AboutDiv>
       </Div1>
-      
+
       <Div2 className='about'>
         <A name='about'></A>
         <Title2>about</Title2>
         <AboutDiv>
           <Banner>
-          <HeadshotImg src={Headshot} />
-          <ul>
-            <ListItem><span>Student at DevMountain</span> <Break />Immersive Web-Development Course</ListItem>
-            <ListItem><span>BBA Economics</span> <Break/>University of Georgia</ListItem>
-            <ListItem><span>BS Industrial Engineering</span> <Break/>Texas Tech University</ListItem>
-            <ListItem><span>3 Years as Trainmaster </span> <Break/>Los Angeles and Richmond, CA</ListItem>
-            <ListItem><span>Manager of Logistics</span> <Break/>Philmont Scout Ranch</ListItem>
-          </ul>
-        </Banner>
+            <HeadshotImg src={Headshot} />
+            <ul>
+              <ListItem><span>Student at DevMountain</span> <Break />Immersive Web-Development Course</ListItem>
+              <ListItem><span>BBA Economics</span> <Break />University of Georgia</ListItem>
+              <ListItem><span>BS Industrial Engineering</span> <Break />Texas Tech University</ListItem>
+              <ListItem><span>3 Years as Trainmaster </span> <Break />Los Angeles and Richmond, CA</ListItem>
+              <ListItem><span>Manager of Logistics</span> <Break />Philmont Scout Ranch</ListItem>
+            </ul>
+          </Banner>
         </AboutDiv>
       </Div2>
 
+      <Div1>
+        <Title2>contact</Title2>
+        <ul>
+          <ListItem><A>GitHub</A></ListItem>
+          <ListItem><A>LinkedIn</A></ListItem>
+          <ListItem><A>Email</A></ListItem>
+        </ul>
+      </Div1>
 
 
-      <Footer/>
-   
+
+      <Footer />
+
     </div>
   );
 }
