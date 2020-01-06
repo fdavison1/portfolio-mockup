d5306ac669471d03144e365a4a52f2b518583114
fdavison1 <fred.noreply@gmail.com>
2020-01-06 15:51:08 -0700

    hosting

--- server/server.js
+++ server/server.js
@@ -6,10 +6,11 @@ const { SERVER_PORT } = process.env
 const app = express()
 app.use(express.json())
 
-app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../build/index.html'));
-});
 
 app.use( express.static( `${__dirname}/../build` ) );
+
+    app.get('*', (req, res)=>{
+        res.sendFile(path.join(__dirname, '../build/index.html'));
+    });
 
 app.listen(SERVER_PORT, () => console.log(`port ${SERVER_PORT} is on and ready to copy`))
